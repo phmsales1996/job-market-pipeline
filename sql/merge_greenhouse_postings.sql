12f6aeab940e66a09caf7b42f58ec712bfaf6ffc
Paulo Sales <138885973+phmsales1996@users.noreply.github.com>
2026-09-24 16:15:53 -0300

    Make the merge refuse to overwrite new data

--- sql/merge_greenhouse_postings.sql
+++ sql/merge_greenhouse_postings.sql
@@ -4,8 +4,8 @@ USING (
     FROM `{dataset}.greenhouse_postings_incoming`
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY landed_at DESC) = 1
 ) AS S
-ON T.id = S.id
-WHEN MATCHED THEN
+ON T.id = S.id 
+WHEN MATCHED and T.last_seen_at < S.last_seen_at THEN
     UPDATE SET
     T.title = S.title,
     T.url = S.url,
@@ -20,6 +20,9 @@ WHEN MATCHED THEN
     T.deadline_at = S.deadline_at,
     T.raw = S.raw,
     T.last_seen_at = S.last_seen_at
+WHEN MATCHED and T.first_seen_at > S.first_seen_at THEN
+    UPDATE SET
+    T.first_seen_at = S.first_seen_at
 WHEN NOT MATCHED THEN
     INSERT (id, title, url, location, company, department, office, language, content, updated_at, published_at, deadline_at, raw, first_seen_at, last_seen_at)
     VALUES (S.id, S.title, S.url, S.location, S.company, S.department, S.office, S.language, S.content, S.updated_at, S.published_at, S.deadline_at, S.raw, S.first_seen_at, S.last_seen_at)
